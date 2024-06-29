fd690719edea6a8929b450896f689bd3dc5bd678
Hitesh Reddy <70234449+debugger-ctrl@users.noreply.github.com>
2024-06-29 14:43:50 -0500

    Initial Commit

---
+++ Meal_App/Meal_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Meal_AppApp.swift
+//  Meal_App
+//
+//  Created by Hitesh Reddy on 29/06/24.
+//
+
+import SwiftUI
+
+@main
+struct Meal_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
